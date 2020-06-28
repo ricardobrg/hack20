c81bcea08d076b779a14f5b9a2fc6153cafa9db6
nsj09 <nsj09@hotmail.com>
2020-06-28 14:56:34 -0300

    quase todo o codigo funcionado da home falta fazer a pesquisa do canal

---
+++ lib/home/channel_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChannelPage extends StatelessWidget{
+
+  Widget build(BuildContext coontext){
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('estou aqui'),
+        ],
+      ),
+    );
+  }
+}
